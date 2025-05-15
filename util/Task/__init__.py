5f166a0d5056b913d1d1fe60182a4a63d122f7e5
J1uShan9 <jiushang1124@gmail.com>
2025-05-15 19:12:20 +0800

    fix: queryticket sleep/2 -> sleep/1.5

--- util/Task/__init__.py
+++ util/Task/__init__.py
@@ -519,13 +519,13 @@ class Task:
                     logger.info("【等待余票】暂时无票, 持续查询票仓中...")
                     self.availableTime = 0
                     # 刷新
-                    sleep(self.sleep / 2)
+                    sleep(self.sleep / 1.5)
 
             # 不知道
             case _:
                 logger.error(f"【等待余票】{code}: {msg}")
                 # 刷新
-                sleep(self.sleep / 2)
+                sleep(self.sleep / 1.5)
 
     @logger.catch
     def CreateOrderAction(self) -> None:
